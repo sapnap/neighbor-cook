5b6b34219fe88e2997ba61fad08c625d5e874852
Sammy Nguyen <smnguyen@stanford.edu>
2014-02-05 19:38:57 -0800

    Setup routes

--- app.js
+++ app.js
@@ -5,12 +5,13 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-var handlebars = require('express3-handlebars')
+var handlebars = require('express3-handlebars');
 
 var index = require('./routes/index');
-var project = require('./routes/project');
-// Example route
-// var user = require('./routes/user');
+var profile = require('./routes/profile');
+var messages = require('./routes/messages');
+var search = require('./routes/messages');
+var friends = require('./routes/friends');
 
 var app = express();
 
@@ -36,10 +37,11 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+app.get('/profile', profile.view);
+app.get('/messages', messages.view);
+app.get('/search', search.view);
+app.get('/friends', friends.view);
 app.post('/auth/facebook/callback', index.login);
-app.get('/project/:name', project.viewProject);
-// Example route
-// app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -3,32 +3,12 @@
  * GET home page.
  */
 
-exports.view = function(req, res){
-  console.log('index.js file');
-  res.render('index', {
-    'myvar': 'sfdasdf', 
-    'projects': [
-      { 'name': 'HELLLOOOOO',
-        'image': 'lorempixel.people.1.jpeg',
-        'id': 'project1'
-      },
-      { 'name': 'Needfinding',
-        'image': 'lorempixel.city.1.jpeg',
-        'id': 'project2'
-      },
-      { 'name': 'Prototyping',
-        'image': 'lorempixel.technics.1.jpeg',
-        'id': 'project3'
-      }
-    ]  
-  });
+exports.view = function(req, res) {
+  res.render('index');
 };
 
-exports.login = function(req, res){
-  console.log('logging user in');
+exports.login = function(req, res) {
   var authResponse = req.params;
   console.log(authResponse);
-  res.render('index', {
-    'myvar': 'sfdasdf'  
-  });
+  res.render('index');
 };
