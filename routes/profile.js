988b9f4ea55973a64f1700b89d0541d07323233e
Rahul Pandey <rpandey1234@gmail.com>
2014-02-06 14:02:30 -0800

    flesh out profile page

--- routes/profile.js
+++ routes/profile.js
@@ -1,10 +1,33 @@
 
 exports.viewSelf = function(req, res) {
-    var data = { 'editable': true };
+    var data = { 'editable': true,
+    	'name': 'Sammy Patel',
+    	'location': 'Stanford, CA',
+    	'donated': 19,
+    	'received': 4,
+    	'inventory': [
+    		{'id': 1, 'name': 'Apple'},
+    		{'id': 2, 'name': 'Banana'},
+    		{'id': 3, 'name': 'Milk'},
+    		{'id': 4, 'name': 'Salt'},
+    	] 
+     };
     res.render('profile', data);
 };
 
 exports.view = function(req, res) {
     var userID = req.params.id;
-    res.render('profile');
+    var data = { 'editable': false,
+    	'name': 'Sammy Patel',
+    	'location': 'Stanford, CA',
+    	'donated': 19,
+    	'received': 4,
+    	'inventory': [
+    		{'id': 1, 'name': 'Apple'},
+    		{'id': 2, 'name': 'Banana'},
+    		{'id': 3, 'name': 'Milk'},
+    		{'id': 4, 'name': 'Salt'},
+    	] 
+     };
+    res.render('profile', data);
 };
