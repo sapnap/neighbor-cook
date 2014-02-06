5328a4210af8691d2d0c513da8e23c4cd9bb6c8f
Rahul Pandey <rpandey1234@gmail.com>
2014-02-06 14:49:28 -0800

    flesh out profile and search pages

--- routes/search.js
+++ routes/search.js
@@ -1,4 +1,18 @@
 
 exports.view = function(req, res) {
-    res.render('search');
+		var query = req.query.query;
+		if (query === undefined) {
+			res.render('search');
+		} else {
+			console.log(query);
+			data = {
+				'query': query,
+				'results': [
+					{'name': 'Rahul', 'id': '1', 'location': 'Stanford, CA', 'quantity': 3},
+					{'name': 'Sammy', 'id': '2', 'location': 'Menlo Park, CA', 'quantity': 1},
+					{'name': 'Sapna', 'id': '3', 'location': 'Stanford, CA', 'quantity': 0},
+				]
+			};
+			res.render('search', data);
+		}
 };
